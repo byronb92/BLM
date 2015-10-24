43ed9c09b360492d591bb31ece058e4bbf6cbc59
Byron Becker <byronb92@thetreaurechest.westell.com>
2015-10-24 17:35:46 -0400

    Successfully implemented DataParser class to parse champion stats. Updated Champion class to set base stats for a specified champion. I chose not to upload the champion library as it is rather large. Third-party JSON library has also been used, but not added to repository yet.

--- src/Champion.java
+++ src/Champion.java
@@ -1,3 +1,4 @@
+import org.json.simple.JSONObject;
 
 public class Champion 
 {
@@ -32,28 +33,17 @@ public class Champion
 	private double attackSpeed;
 	
 	/**
-	 * Retrieve base stats from champion database, given by champion base name.
+	 * Retrieve base stats from champion JSON files, given by champion base name.
 	 * @param championName
 	 */
 	public Champion(String championName)
 	{
-		
+		DataParser dataParser = new DataParser();
+		JSONObject championStats = dataParser.getChampionStats(championName);
+		setBaseStats(dataParser, championStats);
 	}
 	
-	/**
-	 * Set up a champion based on base statistics and perform math based on level.
-	 * @param baseHP
-	 * @param baseAD
-	 * @param baseAS
-	 * @param baseArmor
-	 * @param baseMR
-	 * @param currentLEvel
-	 */
-	public Champion(double baseHP, double baseAD, double baseAS,
-			double baseArmor, double baseMR, int currentLevel)
-	{
-		
-	}
+	
 	
 	/**
 	 * Ensure statistics match the current level of the champion.
@@ -68,12 +58,46 @@ public class Champion
 		attackSpeed = baseAS + (asPerLevel * (currentLevel - 1)) + bonusAS;
 	}
 	
-	
+	public int getLevel()		{ return currentLevel; }
 	public double getHP() 		{ return hitPoints; }
 	public double getAD() 		{ return attackDamage; }
 	public double getAP() 		{ return abilityPower; }
 	public double getArmor() 	{ return armor; }
 	public double getMR() 		{ return magicResistance; }
 	public double getAS() 		{ return attackSpeed; }
+	
+	
+	private void setBaseHP(double hp) { baseHP = hp; }
+	private void setBaseAD(double ad) { baseAD = ad; }
+	private void setBaseArmor(double armor) { baseArmor = armor; }
+	private void setBaseMR(double mr) { baseMR = mr; }
+	private void setBaseAS(double as){ baseAS = as; }
+	
+	private void setBaseHPPerLevel(double hp) { hpPerLevel = hp; }
+	private void setBaseADPerLevel(double ad) { adPerLevel = ad; }
+	private void setBaseArmorPerLevel(double armor) { armPerLevel = armor; }
+	private void setBaseMRPerLevel(double mr) { mrPerLevel = mr; }
+	private void setBaseASPerLevel(double as){ asPerLevel = as; }
+	
+	/**
+	 * Method used in constructor to make things more readable.
+	 * @param data	DataParser class used to get data for specified champion.
+	 * @param championStats JSON Object obtained from champion specific name.
+	 */
+	private void setBaseStats(DataParser data, JSONObject championStats)
+	{
+		setBaseHP(data.getHP(championStats).doubleValue());
+		setBaseAD(data.getAD(championStats).doubleValue());
+		setBaseArmor(data.getArmor(championStats).doubleValue());
+		setBaseMR(data.getMR(championStats).doubleValue());
+		setBaseAS(data.getAS(championStats).doubleValue());	
+		
+		setBaseHPPerLevel(data.getHPPerLevel(championStats).doubleValue());
+		setBaseADPerLevel(data.getADPerLevel(championStats).doubleValue());
+		setBaseArmorPerLevel(data.getArmorPerLevel(championStats).doubleValue());
+		setBaseMRPerLevel(data.getMRPerLevel(championStats).doubleValue());
+		setBaseASPerLevel(data.getASPerLevel(championStats).doubleValue());
+	}
+	
 
 }
---
+++ src/DataParserTest.java
@@ -0,0 +1,48 @@
+import junit.framework.TestCase;
+import org.json.simple.JSONObject;
+
+public class DataParserTest extends TestCase {
+
+	JSONObject championStats;
+	protected void setUp() throws Exception {
+		super.setUp();
+		DataParser dataparser = new DataParser();
+		championStats = dataparser.getChampionStats("Alistar");
+	}
+	
+
+			
+	// Test stat retrieval with Alistar example. (Patch 5.18)
+	/**
+	 *      "hp":613.36,
+            "hpperlevel":106.0,
+            "mp":278.84,
+            "mpperlevel":38.0,
+            "movespeed":330.0,
+            "armor":24.38,
+            "armorperlevel":3.5,
+            "spellblock":32.1,
+            "spellblockperlevel":1.25,
+            "attackrange":125.0,
+            "hpregen":8.675,
+            "hpregenperlevel":0.85,
+            "mpregen":6.0,
+            "mpregenperlevel":0.8,
+            "crit":0.0,
+            "critperlevel":0.0,
+            "attackdamage":61.1116,
+            "attackdamageperlevel":3.62,
+            "attackspeedoffset":0.0,
+            "attackspeedperlevel":2.125
+	 */
+	public void testHP()
+	{
+		Double alistarHP = (Double)championStats.get("hp");
+		assertEquals(alistarHP.doubleValue(), 613.36, 0);
+	}
+	
+	
+			
+
+
+}
